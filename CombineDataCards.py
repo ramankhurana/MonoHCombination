15b06005d85183207aed8168f0f190dd5d026039
Raman Khurana <raman.khurana@cern.ch>
2016-11-29 03:53:56 -0600

    modified python scripts

--- CombineDataCards.py
+++ CombineDataCards.py
@@ -39,8 +39,8 @@ for imass in range(len(massvec)):
         datacards={
             'WW': 'WW/Datacards.txt ',
             'gg': 'gg/Datacards.txt ',
-            'tt': 'tt/Datacards.txt ',
-            'bb': 'bb/Datacards.txt ', 
+            'tt': 'tt/xtt_cards/Zprime'+str(massvec[imass])+'A'+str(ia0mass)+'/cmb/'+str(ia0mass)+'/DataCard_2HDM_M'+str(massvec[imass])+'_'+str(ia0mass)+'GeV_MonoHTauTau_13TeV.txt ',
+            'bb': 'bb/resolved/ZprimeToA0hToA0chichihbb_2HDM_MZp'+str(massvec[imass])+'_MA0'+str(ia0mass)+'_13TeVmadgraphDatacards/ZprimeToA0hToA0chichihbb_2HDM_MZp'+str(massvec[imass])+'_MA0'+str(ia0mass)+'_13TeVmadgraph_comb_v2.txt ', 
             'ZZ': 'ZZ/Datacards.txt '
             }
         
@@ -58,7 +58,7 @@ for imass in range(len(massvec)):
         tmpdcard = 'tmpcard.txt'
         if (len(sys.argv) >= 2) & (not ('runlimit' in sys.argv )) :
             
-            print "os.system('combineCards.py  '+allcards+' >& '+tmpdcard)"
+            os.system('combineCards.py  '+allcards+' >& '+datacardnamefit)
             #os.system('cat '+tmpdcard+' '+rateparm+' >& '+  datacardnamefit)
             
         nargv = len(sys.argv)
