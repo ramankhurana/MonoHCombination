652b693165fae7617a0b9b6364fded835da74070
Raman Khurana <raman.khurana@cern.ch>
2016-12-02 03:14:51 -0600

    adding comparuson plot macro

---
+++ AllFinalStates/DivideByBR.C
@@ -0,0 +1,31 @@
+void DivideByBR(){
+  TString filename = "gg.root";
+  float br = 1.0;
+  bool prodxs = false;
+  float xs[]={0.45217, 0.27765, 0.14383, 0.075451, 0.041208, 0.017786, 0.0082317, 0.0025458};
+  
+  TFile* f = new TFile (filename, "update");
+  f->cd();
+  TGraph* limitgr = (TGraph*) f->Get("limit");
+  std::cout<<" # of points = "<<limitgr->GetN()<<std::endl;
+  const int npoint = 8;
+  Double_t* x;
+  Double_t* y;
+  x = limitgr->GetX();
+  y = limitgr->GetY();
+
+
+  for (int i=0; i<npoint ;i++){
+    y[i]  = y[i] / br ;
+    if (prodxs) y[i] = y[i] / xs[i];
+    std::cout<<" y = "<<y[i]<<std::endl;
+  }
+  
+  TGraph* limit_  = new TGraph(npoint, x, y);
+  limit_ = limitgr;
+  limit_->SetName("limit_");
+  limit_->Draw();
+  limit_->Write();
+  
+  
+}
