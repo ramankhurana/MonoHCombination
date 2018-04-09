5b65a0ea0ce65118c398d668349444a2a4f892a2
Raman Khurana <ramankhurana1986@gmail.com>
2018-04-09 15:18:31 +0200

    first version of runcombo

---
+++ config_combo.py
@@ -0,0 +1,61 @@
+#####path to combo input and combo cards 
+monohCombo={
+## 2HDM 
+'ggpath2HDM':'combocards/gg_2HDM/',
+'tautaupath2HDM':'combocards/MB_2HDM_tt/',
+'wwpath2HDM':'combocards/datacards_combination/monoH_MVA_em/muccamva2HDMadaptFull_All_Bin800/',#combocards/muccamva2HDMadaptFull_All_Bin800/',
+'bbpath2HDM':'combocards/bb_2HDM/datacards/',
+
+## Z' Baryonic 
+'ggpathZPB':'combocards/gg_BARY/',
+'tautaupathZPB':'combocards/MB_ZpB_tt/',
+'wwpathZPB':'combocards/datacards_combination/monoH_MVA_em/muccamvaZbaradaptFull_All_Bin100/',
+'bbpathZPB':'combocards/',
+
+
+## full sim samples mass point
+'FSmasspoints2HDM':'bin/FS2HDM.txt',
+'FSmasspointsZPB':'bin/FSZPB.txt',
+
+## full sim sample mass points, when all final state have it. 
+'allAvailable2HDM':'bin/FSAll2HDM.txt',
+'allAvailableZPB':'bin/FSAllZPB.txt',
+
+## full sim sample mass points, when at least bb final state have it.
+'bbAvailable2HDM':'bin/FSbb2HDM.txt',
+'bbAvailableZPB':'bin/FSbbZPB.txt',
+
+## full sim mass points and status of availability for each final state 
+'statusAll2HDM':'bin/statusAll2HDM.txt',
+'statusAllZPB':'bin/statusAllZPB.txt',
+
+## name of all the datacards for combo 
+'combocards2HDM':'bin/combocardsName2HDM.txt',
+'combocardsZPB':'bin/combocardsNameZPB.txt',
+
+## name of all individual cards
+'ggcards2HDM':'bin/ggcards2HDM.txt',
+'ttcards2HDM':'bin/ttcards2HDM.txt',
+'wwcards2HDM':'bin/wwcards2HDM.txt',
+'bbcards2HDM':'bin/bbcards2HDM.txt',
+
+'ggcardsZPB':'bin/ggcardsZPB.txt',
+'ttcardsZPB':'bin/ttcardsZPB.txt',
+'wwcardsZPB':'bin/wwcardsZPB.txt',
+'bbcardsZPB':'bin/bbcardsZPB.txt',
+
+## datacards names
+'gg_cardname2HDM':'dataCard_sig_2HDM_mZPXXX_mA0YYY_13TeV.txt',
+'tautau_cardname2HDM':'ZprimeXXXAYYY/cmb/YYY/DataCard_2HDM_MXXX_YYYGeV_MonoHTauTau_13TeV.txt',
+'ww_cardname2HDM':'datacard_XXX_YYY_combined.txt',
+'bb_cardname2HDM':'monoHnn_MZXXX_MAYYY.txt',
+
+'gg_cardnameZPB':'',
+'tautau_cardnameZPB':'',
+'ww_cardnameZPB':'',
+'bb_cardnameZPB':'',
+
+'combocardname2HDM':'combocards/combo_gg_ww_tt_bb_2HDM/Datacard_MZpXXXMa0YYYMonoHCombo2016FullData.txt',
+'combocardnameZpB':'combocards/combo_gg_ww_tt_bb_ZpB/Datacard_MZpXXXMa0YYYMonoHCombo2016FullData.txt',
+
+}
